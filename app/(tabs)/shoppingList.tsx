7bb12e274440a92495ba7fd02a3ca885bccdee4d
Furkan Erguldurenler <furkanerguldurenler@hotmail.com>
2025-04-26 16:46:43 +0300

    adding sharing

--- app/(tabs)/shoppingList.tsx
+++ app/(tabs)/shoppingList.tsx
@@ -1,10 +1,28 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import useMealStore from "../stores/mealStore";
+import * as Sharing from 'expo-sharing';
 import { t } from "i18next";
 
 const ShoppingListPage = () => {
     const { shoppingList, removeFromShoppingList } = useMealStore();
+    const shoppingListText = shoppingList.join("\n");
+
+    const handleShare = async () => {
+        const isAvailable = await Sharing.isAvailableAsync();
+        if (isAvailable) {
+            try {
+                await Sharing.shareAsync(shoppingListText, {
+                    dialogTitle: 'Alışveriş Listesini Paylaş',
+                    UTI: 'public.text',
+                });
+            } catch (error) {
+                console.log("Paylaşım hatası:", error);
+            }
+        } else {
+            alert("Paylaşım özelliği desteklenmiyor.");
+        }
+    };
 
     return (
         <View style={styles.container}>
@@ -24,6 +42,15 @@ const ShoppingListPage = () => {
                         </TouchableOpacity>
                     </View>
                 ))
+            )}
+
+            {shoppingList.length > 0 && (
+                <TouchableOpacity
+                    style={styles.shareButton}
+                    onPress={handleShare}
+                >
+                    <Text style={styles.shareText}>Paylaş</Text>
+                </TouchableOpacity>
             )}
         </View>
     );
@@ -71,6 +98,18 @@ const styles = StyleSheet.create({
     deleteText: {
         color: "#fff",
         fontWeight: "bold",
+    },
+    shareButton: {
+        marginTop: 20,
+        backgroundColor: "#4CAF50",
+        paddingVertical: 12,
+        borderRadius: 5,
+        alignItems: "center",
+    },
+    shareText: {
+        color: "#fff",
+        fontWeight: "bold",
+        fontSize: 18,
     },
 });
 
