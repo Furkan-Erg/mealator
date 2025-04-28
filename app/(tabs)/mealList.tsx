9ea8c19762c4d972b66157700765bcb430cdbc8b
Furkan Erguldurenler <furkanerguldurenler@hotmail.com>
2025-04-28 18:11:44 +0300

    adding fuzzy search for filter meallist

--- app/(tabs)/mealList.tsx
+++ app/(tabs)/mealList.tsx
@@ -1,31 +1,46 @@
-import { Input, ScrollView, View, XStack, Text } from "tamagui";
-import React from "react";
+import { Input, ScrollView, View, XStack, Text, debounce } from "tamagui";
+import React, { useState, useCallback } from "react";
 import { CardComponent } from "../components/CardComponent";
 import useMealStore from "../stores/mealStore";
 import { t } from "i18next";
 
 const MealListPage = () => {
     const { mealList } = useMealStore();
+    const [searchText, setSearchText] = useState("");
+
+    const handleInputChange = useCallback(
+        debounce((text: string) => {
+            setSearchText(text);
+        }, 500),
+        []
+    );
+
+    const filteredMeals = mealList.filter((meal) =>
+        meal.name.toLowerCase().includes(searchText.toLowerCase())
+    );
 
     return (
         <View style={styles.container}>
-            <XStack alignItems="center" width={'100%'} paddingHorizontal={15} marginTop={40}
-            >
+            <XStack alignItems="center" width={'100%'} paddingHorizontal={15} marginTop={40}>
                 <Text fontSize={30} fontWeight={'bold'} color={'$color'}>{t('mealList')}</Text>
             </XStack>
-            <XStack alignItems="center" width={'100%'} paddingHorizontal={15} marginTop={20}
-                marginBottom={20}>
-                <Input flex={1} size={'$4'} placeholder={t('mealSearchPlaceholder')} />
+            <XStack alignItems="center" width={'100%'} paddingHorizontal={15} marginTop={20} marginBottom={20}>
+                <Input
+                    flex={1}
+                    size={'$4'}
+                    placeholder={t('mealSearchPlaceholder')}
+                    onChangeText={(text) => handleInputChange(text)}
+                />
             </XStack>
-            <ScrollView showsVerticalScrollIndicator={false}  >
-                <CardComponent list={mealList} ></CardComponent>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <CardComponent list={filteredMeals}></CardComponent>
             </ScrollView>
-        </View >
-
+        </View>
     );
 };
 
 export default MealListPage;
+
 const styles = {
     container: {
         flex: 1,
@@ -33,5 +48,4 @@ const styles = {
         alignItems: "center",
         padding: 20,
     },
-
 };
